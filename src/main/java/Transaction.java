5bdb0280eaa2dba7c24a8640985f4947caea743d
LeDfee <yermakovdo@gmail.com>
2024-03-19 18:27:31 +0100

    Update Transaction.java

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,2 +1,23 @@
+import java.util.Date;
+
 public class Transaction {
+    private String transactionId;
+    private TransactionType type;
+    private double amount;
+    private Date date;
+
+    public Transaction(String transactionId, TransactionType type, double amount) {
+        this.transactionId = transactionId;
+        this.type = type;
+        this.amount = amount;
+        this.date = new Date();
+    }
+
+    @Override
+    public String toString() {
+        return "Идентификатор транзакции: " + transactionId +
+                ", Дата: " + date +
+                ", Тип: " + type +
+                ", Сумма: " + amount;
+    }
 }
